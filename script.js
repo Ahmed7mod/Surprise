79d9681e9e9e0e1d38733964c9bc6b97089135e6
Ahmed7mod <155329080+Ahmed7mod@users.noreply.github.com>
2024-12-10 00:35:29 +0300

    script.js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-document.getElementById('explodeBtn').addEventListener('click', function() {
-    setInterval(() => createHearts(), 10);  // كل 10 ملي ثانية يظهر قلب جديد
-});
-
-document.body.addEventListener('mousemove', function(event) {
+document.getElementById('explodeBtn').addEventListener('click', function(event) {
+    // عند الضغط على الزر، القلوب ستظهر في المنتصف.
     createHearts(event.clientX, event.clientY);
 });
 
-function createHearts(x = null, y = null) {
+document.body.addEventListener('mousemove', function(event) {
+    // عند تحريك الفأرة، القلوب ستظهر في مكان الفأرة.
+    createHearts(event.clientX, event.clientY);
+});
+
+function createHearts(x, y) {
     const heartsContainer = document.getElementById('hearts');
     const heart = document.createElement('div');
     heart.classList.add('heart');
     
-    heart.style.left = `${x ? x - 20 : Math.random() * window.innerWidth}px`;
-    heart.style.top = `${y ? y - 20 : Math.random() * window.innerHeight}px`;
+    // تحديد مكان ظهور القلوب بناءً على مكان الضغط أو تحريك الفأرة.
+    heart.style.left = `${x - 20}px`;
+    heart.style.top = `${y - 20}px`;
     
     heartsContainer.appendChild(heart);
 }
